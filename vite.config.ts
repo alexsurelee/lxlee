80fdf0dbfebe68524f4846f73d5152ca521982e1
Alexander Lee <alexgrlee@gmail.com>
2025-07-19 10:23:08 +1200

    ensure frontmatter is hidden

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import mdx from "@mdx-js/rollup";
 import rehypeHighlight from "rehype-highlight";
+import remarkFrontmatter from "remark-frontmatter";
 
 export default defineConfig({
   plugins: [
-    mdx({ rehypePlugins: [rehypeHighlight] }),
+    mdx({
+      remarkPlugins: [remarkFrontmatter],
+      rehypePlugins: [rehypeHighlight],
+    }),
     reactRouter(),
     tsconfigPaths(),
   ],
